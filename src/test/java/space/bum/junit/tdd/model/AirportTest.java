d01543e74cb688c02a1ceb9431e518f3dacbf59c
osparking <jbpark03@gmail.com>
2024-02-09 12:04:26 +0900

    일반 항공편 시험 - 2/3 단계 - 보통 승객

--- src/test/java/space/bum/junit/tdd/model/AirportTest.java
+++ src/test/java/space/bum/junit/tdd/model/AirportTest.java
@@ -1,8 +1,11 @@
 package space.bum.junit.tdd.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 class AirportTest {
 
@@ -10,10 +13,22 @@ class AirportTest {
   @Nested
   class EconomyFlightTest {
     private Flight economyFlight;
-    
+
     @BeforeEach
     void setUp() {
       economyFlight = new Flight("1", "Economy");
+    }
+
+    @Test
+    public void testEconomyRegular() {
+      Passenger kim = new Passenger("김", false);
+      assertEquals("1", economyFlight.getId());
+      assertEquals(true, economyFlight.addPassenger(kim));
+      assertEquals(1, economyFlight.getPassengerList().size());
+      assertEquals("김", economyFlight.getPassengerList().get(0).getName());
+      assertEquals(true, economyFlight.removePassenger(kim));
+      assertEquals(0, economyFlight.getPassengerList().size());
+
     }
   }
 
